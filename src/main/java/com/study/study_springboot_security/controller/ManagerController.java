86d19b1cf6e9265454a657d40aaffd21064d7d55
dwogus6893 <dwogus6893@naver.com>
2023-02-10 14:49:23 +0900

    SSS

---
+++ src/main/java/com/study/study_springboot_security/controller/ManagerController.java
@@ -0,0 +1,28 @@
+package com.study.study_springboot_security.controller;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ManagerController {
+   
+    
+    @GetMapping({"/admin/*"})     // ROLE_ADMIN
+    public ModelAndView admin(ModelAndView modelAndView){
+        String viewName = "/WEB-INF/views/admin.jsp";
+        modelAndView.setViewName(viewName);
+        return modelAndView;
+    }
+
+      
+    @GetMapping({"/manager/*"})     // ROLE_MANAGER or ROLE_ADMIN
+    public ModelAndView manager(ModelAndView modelAndView){
+        String viewName = "/WEB-INF/views/admin.jsp";
+        modelAndView.setViewName(viewName);
+        return modelAndView;
+    }
+}
+
